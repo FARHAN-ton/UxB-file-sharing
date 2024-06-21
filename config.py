8c99c48c1a84266fa45fecafde3b30fdf46f44ce
FARHAN-ton <mdi445736@gmail.com>
2024-06-21 18:23:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,70 +1,65 @@
 #(©)CodeXBotz
 #By @Codeflix_Bots
 
-
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-#Bot token @Botfather
+# Bot token from @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6551387262:AAHFwkGUr06f6YDx1XxZHlANq2E1pYkWpHw")
 
-#Your API ID from my.telegram.org
+# API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "22322696"))
 
-#Your API Hash from my.telegram.org
+# API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "9fd68dd28f51acaaa991092879899b1b")
 
-#Your db channel Id
+# DB channel ID
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002184809306"))
 
-#OWNER ID
+# Owner ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6030420429"))
 
-#Port
+# Port
 PORT = os.environ.get("PORT", "8080")
 
-#Database 
+# Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Urlupload_3bot:I0qisgnMyuRXxIpj@cluster0.fa3fgma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Urlupload_3bot")
 
-#force sub channel id, if you want enable force sub
+# Force sub channel IDs
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002118741689"))
 FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002032781980"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-#token varibles
-# my shortner https://dashboard.shareus.io/signup/lifetime/U9AZbV
-
+# Token variables
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "Modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "1b200507341bd8a9bb9c5367413db95d1252b73a")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400))  # Time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
+TUT_VID = os.environ.get("TUT_VID", "gojfsi/2")
 
-#start message
+# Start message
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n ɪ ᴀᴍ ᴍᴜʟᴛɪ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ , ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ » @ultroidofficial</b>")
+
 try:
-    ADMINS=[6030420429]
-    for x in (os.environ.get("ADMINS", "6030420429").split()):
+    ADMINS = [6030420429]
+    for x in os.environ.get("ADMINS", "6030420429").split():
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
+# Force sub message
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐒𝐨𝐫𝐫𝐲 {first} 𝐁𝐫𝐨/𝐒𝐢𝐬 𝐲𝐨𝐮 𝐡𝐚𝐯𝐞 𝐭𝐨 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐭𝐨 𝐚𝐜𝐜𝐞𝐬𝐬 𝐟𝐢𝐥𝐞𝐬..\n\n 𝐒𝐨 𝐩𝐥𝐞𝐚𝐬𝐞 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐚𝐧𝐝 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 “𝐍𝐨𝐰 𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞” 𝐛𝐮𝐭𝐭𝐨𝐧....!")
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
+# Custom caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>» ʙʏ @ultroidxTeam</b>")
 
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+# Protect content
+PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "True") == "True"
 
-#Set true if you want Disable your Channel Posts Share button
+# Disable channel button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
@@ -89,7 +84,6 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
